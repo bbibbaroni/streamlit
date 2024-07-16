5c8842a4d93ee1a220826b1421d079efcb71aaa6
bbibbaroni <i89155345@gmail.com>
2024-07-16 16:31:56 +0900

    first commit

---
+++ pages/2_Result.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.title("Result")
+
+text = '''
+---
+'''
+
+st.markdown(text)
+
+if 'number' in st.session_state: 
+    st.write("학번 : ",st.session_state.number)
+if 'name' in st.session_state: 
+    st.write("이름 : ", st.session_state.name)
+
+if 'name' in st.session_state: 
+    st.write(st.session_state.name,"님의 최종결과는", "점 입니다.")
---
+++ pages/3_Test.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+# Fetch the service account key JSON file contents
+cred = credentials.Certificate('import.json')
+
+# Initialize the app with a service account, granting admin privileges
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://test-aa-4297c-default-rtdb.firebaseio.com/'
+})
+
+# As an admin, the app has access to read and write all data, regradless of Security Rules
+#ref = db.reference('Number')
+#st.write(ref.get())
+#ref2 = db.reference('Name')
+#st.write(ref2.get())
+
+ref = db.reference('test')
+ref.set({"hi":"hello"})
+
+ref = db.reference('test2')
+ref.set({"hi":{
+                "hello":"bye",
+                'good':'bad'
+            }
+        })
+
+if st.button("updateTest"):
+    ref = db.reference('test')
+    ref.set({"h1":"HELLO",
+                "bye": "GOOD BYE"})
